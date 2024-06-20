b2581387c346ea5742edb239975bbf560450e251
mukh4mmadov <omuhammadov467@gmail.com>
2024-06-20 23:36:36 +0500

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://t.me/mukh4mmadov">Ozodbek Muhammadov</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
